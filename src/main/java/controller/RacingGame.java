56e156dd516eff6b16ba392f62b14defa2efe5af
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-16 15:33:09 +0900

    feat(InputView): 시도할 횟수 입력받기

--- src/main/java/controller/RacingGame.java
+++ src/main/java/controller/RacingGame.java
@@ -7,17 +7,18 @@ import model.Car;
 import view.InputView;
 
 public class RacingGame {
-	private static List<Car> carList;
-	
+	private List<Car> carList;
+	private int tryNumber;
 	public RacingGame() {
 		carList = new ArrayList<>();
+		tryNumber = 0;
 	}
 	
-	public static void play() {
+	public void play() {
 		String[] carNames = InputView.readCarNames().split(",");
 		for(String carName : carNames) {
 			carList.add(Car.createCar(carName));
 		}
-		
+		tryNumber = InputView.readTryNumber();
 	}
 }
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,7 @@ import controller.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-    	RacingGame.play();
+    	RacingGame racingGame = new RacingGame();
+    	racingGame.play();
     }
 }
