e037585ff3ec1b5c69fc2ef3271b187ad5b2b155
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-16 15:41:47 +0900

    feat(RacingGame): 잘못된 횟수 입력일 경우 재입력받는 기능

--- src/main/java/controller/RacingGame.java
+++ src/main/java/controller/RacingGame.java
@@ -13,12 +13,20 @@ public class RacingGame {
 		carList = new ArrayList<>();
 		tryNumber = 0;
 	}
-	
+
 	public void play() {
 		String[] carNames = InputView.readCarNames().split(",");
 		for(String carName : carNames) {
 			carList.add(Car.createCar(carName));
 		}
-		tryNumber = InputView.readTryNumber();
+		while(true) {
+			try {
+				tryNumber = InputView.readTryNumber();
+				break;
+			}catch(IllegalArgumentException e) {
+				System.out.println("[ERROR]: 시도 횟수는 숫자여야 한다.");
+				continue;
+			}
+		}
 	}
 }
